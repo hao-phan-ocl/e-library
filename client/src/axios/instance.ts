769dc9103e439dca784a9f0bad63826cd22e279c
Hao Phan <nguyenhao.phan@gmail.com>
2022-04-14 22:06:28 +0300

    update axios baseURL

--- client/src/axios/instance.ts
+++ client/src/axios/instance.ts
@@ -3,11 +3,8 @@ import axios from 'axios'
 import { logout } from '../redux/auth/actions'
 import store from '../redux/store'
 
-const prod = process.env.NODE_ENV === 'production'
 const instance = axios.create({
-  baseURL: prod
-    ? 'https://fs9-elibrary.herokuapp.com/api/v1'
-    : 'http://localhost5000/api/v1',
+  baseURL: process.env.REACT_APP_BACKEND,
 })
 
 // Using interceptors to dynamically set the header for each request
