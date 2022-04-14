dfa9e53fa3e762655ee92c45369cf02abebfebcb
Hao Phan <nguyenhao.phan@gmail.com>
2022-04-14 19:06:06 +0300

    update: node and ts package

--- api/src/types/express-flash.d.ts
+++ api/src/types/express-flash.d.ts
@@ -7,13 +7,6 @@
 declare namespace Express {
   export interface Request {
     flash(event: string, message: any): any
-    user?: {
-      _id: string
-      firstName: string
-      lastName: string
-      email: string
-      bookLists?: string[]
-    }
   }
 }
 
