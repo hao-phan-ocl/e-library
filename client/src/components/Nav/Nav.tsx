3c1f555bf9235a8d8a74db8a1dd7e9da1a0dfc3e
Hao Phan <nguyenhao.phan@gmail.com>
2022-05-01 06:26:52 +0300

    change nav

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -30,7 +30,7 @@ export default function Nav() {
               textTransform: 'lowercase',
             }}
           >
-            Hello
+            e
           </Typography>
           <Typography
             color="secondary"
