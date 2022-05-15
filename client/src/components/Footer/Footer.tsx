1f9f4be32b3f2a1a03c9924c8e97b8dc350e4a7d
Hao Phan <nguyenhao.phan@gmail.com>
2022-05-16 00:22:53 +0300

    add portfolio link to footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,16 +1,25 @@
 import { Typography, Link, Stack } from '@mui/material'
-import { GitHub, LinkedIn } from '@mui/icons-material'
+// import { GitHub, LinkedIn } from '@mui/icons-material'
 
 export default function Footer() {
   return (
     <Stack alignItems="center" gap="7px" p="50px 0" mt="auto">
       <Stack flexDirection="row" alignItems="center" gap="5px">
         <Typography fontSize="20px">Developed by</Typography>
-        <Typography fontSize="20px" fontWeight={600}>
-          Hao Phan
-        </Typography>
+        <Link
+          title="My Portfolio"
+          href="https://haophan.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Portfolio"
+          underline="hover"
+        >
+          <Typography color="primary" fontSize="20px" fontWeight={600}>
+            Hao Phan
+          </Typography>
+        </Link>
       </Stack>
-      <Stack flexDirection="row" gap="5px">
+      {/* <Stack flexDirection="row" gap="5px">
         <Link
           href="https://www.linkedin.com/in/hao-phan-06b628110/"
           target="_blank"
@@ -31,7 +40,7 @@ export default function Footer() {
             sx={{ fontSize: '35px', ':hover': { color: 'rgb(233, 30, 99)' } }}
           />
         </Link>
-      </Stack>
+      </Stack> */}
     </Stack>
   )
 }
